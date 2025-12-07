6d76e38282757ed9878d60bec28b6923e8d47ff0
sujonbiswasdev <sujonbiswasdev@gmail.com>
2025-12-07 17:08:20 +0600

    done

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv'
 import path from 'path'
 dotenv.config({path:path.join(process.cwd(),'.env')})
-
 const config={
     port:process.env.PORT,
     db_url:process.env.DATABASE_URL,
--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,18 +2,14 @@ import { NextFunction, Request, Response } from "express"
 import jwt, { JwtPayload } from 'jsonwebtoken'
 import config from "../config/config"
 import { pool } from "../config/DB"
-
-
 const auth=(roles:string[])=>{
     return async(req:Request,res:Response,next:NextFunction)=>{
         const token = req.headers.authorization
-
         if(!token){
              const data= res.status(401).json({sucess:false,message:"you are not authorized"})
                throw new Error(data as any)
                return;
         }
-
         const decoded = jwt.verify(token as string, config.jwt_secret as string) as JwtPayload;
         if(!decoded){
            const data= res.status(400).json({sucess:false,message:"you are not valid"})
