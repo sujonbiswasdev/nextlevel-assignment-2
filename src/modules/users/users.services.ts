c3403708408707ccaa75103a6d169284c5f4f518
sujonbiswasdev <sujonbiswasdev@gmail.com>
2026-02-19 05:24:54 +0600

    update user

--- src/modules/users/users.services.ts
+++ src/modules/users/users.services.ts
@@ -41,7 +41,7 @@ const updateUser=async(userrole:string,id:number,name?:string,email?:string,phon
         `,[updateduser.name,updateduser.email,updateduser.phone,id]
     )
     delete result.rows[0].password
-    return result
+    return result.rows
     }
 }
 
